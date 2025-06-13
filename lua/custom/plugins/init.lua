d08c8a22ce88039f5b491bfd0c834451ed0294c7
Erik Pitzer <erik.pitzer@fh-hagenberg.at>
2025-06-13 15:29:18 +0200

    actually include custom/plugins/init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,1 +1,19 @@
-../../../../../dotfiles/.config/nvim/lua/custom/plugins/init.lua
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
+}
